390a16ac4bfbf120c286b8f973e347f92c19f5c7
doochukbeni <doochukbeni@gmail.com>
2023-10-24 13:42:52 +0400

    set up prisma schema and relation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,6 +12,7 @@ const Navbar = async () => {
   const user = session?.user;
 
   console.log(user);
+  // bestlink paul
 
   return (
     <div className="fixed top-0 inset-x-0  h-fit bg-zinc-100 border-b border-zinc-300 z-10 py-2">
